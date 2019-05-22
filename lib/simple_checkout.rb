a9696b089d56135d441aaccec5d4a48598501871
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-22 12:21:34 +0100

    Simple checkout completed

--- lib/simple_checkout.rb
+++ lib/simple_checkout.rb
@@ -2,12 +2,17 @@ class Checkout
 
   attr_reader :scanned_items, :total
   def initialize
-    @foods = { :cheese => 700, :tea => 250, :bacon => 300, :sausages => 600 }
+    @foods = { :cheese => 0.70, :tea => 0.25, :bacon => 0.30, :sausages => 0.60 }
     @scanned_items = []
     @total = 0
   end
 
   def check_price(check_item_price) 
+    final = @foods[check_item_price]
+    return "£#{('%.2f' % (final))}"
+  end
+
+  def check_price_2(check_item_price) 
     return @foods[check_item_price]
   end
 
@@ -19,9 +24,9 @@ class Checkout
 
   def total_price
     @scanned_items.each { |item|
-      @total += check_price(item).to_i
+      @total += check_price_2(item)
     }
-    return total
+    return "£#{(@total)}"
   end
 
 end
--- spec/simple_checkout_spec.rb
+++ spec/simple_checkout_spec.rb
@@ -3,7 +3,7 @@ require 'simple_checkout'
 describe Checkout do
 
   it '#price - See price of items' do
-    expect(subject.check_price(:cheese)).to eq(700)
+    expect(subject.check_price(:cheese)).to eq("£0.70")
   end
 
   it '#scan_item - See which items have been scanned at checkout' do
@@ -12,12 +12,12 @@ describe Checkout do
 
   it '#total for 2 scanned items' do
     subject.scan([:cheese, :tea])
-    expect(subject.total_price).to eq(950)
+    expect(subject.total_price).to eq("£0.95")
   end
 
   it '#total for all scanned items' do
     subject.scan([:cheese, :tea, :bacon, :sausages])
-    expect(subject.total_price).to eq(1850)
+    expect(subject.total_price).to eq("£1.85")
   end
 
 end
